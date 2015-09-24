9c44457a58db801b3d3ae5ee7d2c6974bf854799
raju_singh <rajumsinghk12@gmail.com>
2015-09-24 15:05:11 +0000

    commit

--- node.js
+++ node.js
@@ -1,5 +1,6 @@
 var routes = require('routes');
 var express = require('express');
+var mongoose = require('mongoose');
 //var bodyParser = require('body-parser');
 
 var stylus = require('stylus');
@@ -15,11 +16,27 @@ function compile(str, path) {
   return stylus(str).set('filename', path);
 }
 
+mongoose.connect('mongodb://localhost/nodejs');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback() {
+  console.log('node js db opened');
+});
+var messageSchema = mongoose.Schema({message: String});
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.find({"message":"Lhellona"}).exec(function(error, messageDoc) {
+  mongoMessage = messageDoc.message;
+});
+
+
 app.get('/partials/*', function(req, res) {
     res.render('../../client/app/' + req.params[0]);
   });
-app.get('/', function(req, res) {
-  res.render('index');
+app.get('*', function(req, res) {
+  res.render('index',{
+    mongoMessage : mongoMessage
+  });
 });/*
 app.get('/', routes.index);
 app.get('/partials/:name', routes.partials);
@@ -31,6 +48,7 @@ app.use(stylus.middleware(
       compile: compile
     }
   ));
+
 
 var port = process.env.PORT;
 app.listen(process.env.PORT, process.env.IP);
